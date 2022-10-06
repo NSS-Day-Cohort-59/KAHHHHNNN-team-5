0c857ef632b1676de9d33e07c076127cd7027da1
Simone Marcantel☆ <simonemarcantel04@gmail.com>
2022-10-06 11:14:18 -0500

    sauces complete

--- scripts/sauces.js
+++ scripts/sauces.js
@@ -10,3 +10,17 @@ document.addEventListener(
         }
     }
 )
+
+export const Sauces = () => {
+    let html = "<ul>"
+
+    for (const sauce of sauces) {
+        html += `<li>
+        <input type="radio" name="sauce" value="${sauce.id}" /> ${sauce.name}
+        </li>`
+
+
+    }
+    html += "</ul>"
+    return html
+}
